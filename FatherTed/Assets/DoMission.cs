2d13275e2c234efedf1ea161f98f4c693bec6bc5
ctrlcookie <regardingquiet@gmail.com>
2022-05-18 15:27:48 +0100

    Lids & Mission Detection
    dw about lid 3 she on her fleabag era

--- FatherTed/Assets/DoMission.cs
+++ FatherTed/Assets/DoMission.cs
@@ -6,8 +6,13 @@ using UnityEngine.UI;
 
 public class DoMission : MonoBehaviour
 {
-    public float MD;
+    public int MD; //missions done
+    public GameObject currentMission;
     public Slider slider;
+
+    public GameObject[] missionEntry;
+
+
     public void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("MissionPoint"))
@@ -23,5 +28,19 @@ public class DoMission : MonoBehaviour
      void Update()
     {
         slider.value = MD;
+
+        currentMission = missionEntry[MD];
+        currentMission.GetComponent<Animator>().SetBool("shouldopen", true);
+
+
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MissionExit") && MD != 0)
+        {
+            missionEntry[MD - 1].GetComponent<Animator>().SetBool("missioncomplete", true);
+
+        }
     }
 }
